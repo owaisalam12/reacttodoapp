39f2520b397fec4a14be2f1c44dd3cc312c17268
Azaan Khan <azaankhan271@gmail.com>
2021-03-12 21:43:45 +0500

    my react todo app assignment

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState }  from 'react'
+import './App.css';
+import ToDoList from './ToDoLists'
+
+
+const App = () => {
+
+  const [inputList, setInputList] = useState("")
+  const [items, setItems] = useState([])
+  const itemEvent = (e) => {
+    setInputList(e.target.value)
+  }
+  const listOfItems = () => {
+  setItems((oldItems)=>{
+    return [...oldItems,inputList]
+    
+  })
+  setInputList("")
+
+  }
+  const deleteItems = (id) => {
+    
+        setItems((oldItems)=>{
+          return oldItems.filter((arrElem,index)=>{
+            return index !== id;
+          })
+        })
+        
+
+    
+        }
+  return (
+  <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1> ToDo List </h1>
+         <br />
+         < input type="text" placeholder="Add Items" onChange={itemEvent}
+         value={inputList}/> 
+         <button onClick={listOfItems}> + </button>
+
+    <ol>
+      {items.map( (itemval,index)=>{
+        return(
+        <ToDoList key={index} id={index} text={itemval} onSelect={deleteItems} />
+        )
+      })}
+    </ol>
+    
+
+    
+    </div>
+
+    </div>
+     </>
+  )
+
+}
+export default App;
